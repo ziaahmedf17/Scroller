6638af4249c4991e5373f8b3bc08588d59682172
Zia Ahmed <ziaahmedf17@gmail.com>
2023-08-13 02:29:20 +0500

    Initial commit

---
+++ app/src/main/java/com/ziaahmedf17/scroller/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ziaahmedf17.scroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
